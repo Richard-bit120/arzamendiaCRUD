557f5821b05d1b47fa904e7fca4c61f795e1ec9f
Richard-bit120 <arzarichard@gmail.com>
2025-06-18 20:44:58 -0300

    update

---
+++ resources/views/tareas/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <span>{{__('Dashboard') }}</span>
+                    <a href="{{ route('tareas.create') }}" class="btn btn-primary btn-sm">Crear Tarea</a>
+                </div>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($tareas as $tarea)
+                                <tr>
+                                    <td>{{ $tarea->id }}</td>
+                                    <td>{{ $tarea->nombre }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
